5a6e2d36bd06405d23ca14db06d73de07e75a634
noor amin <noor.amin@markcoders.com>
2024-07-05 21:35:22 +0500

    nothing too much

---
+++ src/Pages/Layout/DashboardLayout.jsx
@@ -0,0 +1,18 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import BackgroundDashboard from '../../assets/images/BackgroundDashboard.png'
+
+const DashboardLayout = () => {
+  return (
+    <Box
+    sx={{
+       
+    }}
+    >
+
+    </Box>
+  )
+}
+
+export default DashboardLayout
