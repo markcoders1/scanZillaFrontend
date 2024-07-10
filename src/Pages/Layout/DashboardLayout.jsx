f4f01886afe9b6a0c7808ad72321249b70b8d8fe
noor amin <noor.amin@markcoders.com>
2024-07-11 01:02:03 +0500

    done in center

--- src/Pages/Layout/DashboardLayout.jsx
+++ src/Pages/Layout/DashboardLayout.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import AppSidebar from '../AppSidebar/AppSidebar';
@@ -11,58 +11,70 @@ const DashboardLayout = () => {
     <Box
       sx={{
         display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
         minHeight: '100vh',
         backgroundImage: `linear-gradient(rgba(27, 2, 75, .7), rgba(27, 2, 75, .8)), url(${dashboardImg1})`,
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
-        padding:"20px 30px",
+        padding: "20px 30px",
+        boxSizing: 'border-box',
       }}
     >
       <Box
         sx={{
-          width: {
-            lg: '300px',
-            xs: '0px', // Hidden on smaller screens
-           
-          },
-          position: 'fixed',
-          height: '100%',
-          zIndex: 1,
-          display: {
-            xs: 'none',
-            lg: 'block',
-          },
-        }}
-      >
-        <AppSidebar />
-
-      </Box>
-      <Box>
-        <MobileSidebar/>
-      </Box>
-      <Box
-        sx={{
-          marginLeft: {
-            lg: '300px', // Adjust to match sidebar width
-            xs: '0px',
-          },
+          display: 'flex',
           width: '100%',
-          padding: {
-            sm: '50px',
-            xs: '30px 20px',
-          },
-          backgroundColor: 'white',
-          borderRadius: '30px',
+          maxWidth: '1440px',
+          boxSizing: 'border-box',
         }}
       >
-        <Header />
         <Box
           sx={{
-            mt:"20px"
+            width: {
+              lg: '300px',
+              xs: '0px', // Hidden on smaller screens
+            },
+            position: 'relative',
+            height: '100%',
+            zIndex: 1,
+            display: {
+              xs: 'none',
+              lg: 'block',
+            },
           }}
         >
-          <Outlet />
+          <AppSidebar />
+        </Box>
+        <Box>
+          <MobileSidebar />
+        </Box>
+        <Box
+          sx={{
+            marginLeft: {
+              lg: '0px', // Adjust to match sidebar width
+              xs: '0px',
+            },
+            width: '100%',
+            maxWidth: '1240px',
+            padding: {
+              sm: '50px',
+              xs: '30px 20px',
+            },
+            backgroundColor: 'white',
+            borderRadius: '30px',
+            boxSizing: 'border-box',
+          }}
+        >
+          <Header />
+          <Box
+            sx={{
+              mt: "20px"
+            }}
+          >
+            <Outlet />
+          </Box>
         </Box>
       </Box>
     </Box>
