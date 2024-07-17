98ad6a63ba249752f2ff0a8d1777d407f9ecaf15
noor amin <noor.amin@markcoders.com>
2024-07-17 12:24:31 -0700

    tool management complete

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -4,14 +4,23 @@ import axiosInstance from '../../Hooks/useQueryGallery/AuthHook/AuthHook';
 import { Box, Typography, CircularProgress } from '@mui/material';
 import ProfileCard from '../../Components/ProfileCard/ProfileCard';
 import GiftCard from '../../Components/GiftCard/GiftCard';
+import CreditsHistory from '../../Components/CreditsHistory/CreditsHistory';
 
 const appUrl = import.meta.env.VITE_REACT_APP_API_URL;
 
 const Details = () => {
   const { id } = useParams();
   const [details, setDetails] = useState(null);
-  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [creditsHistory, setCreditsHistory] = useState([]);
+  const [analyzeHistory, setAnalyzeHistory] = useState([]);
+
+  const [snackAlertData, setSnackAlertData] = useState({
+    message: "",
+    severity: "success",
+    open: false,
+  });
 
   //   useEffect(() => {
   //     const fetchDetails = async () => {
@@ -63,6 +72,83 @@ const Details = () => {
   //     );
   //   }
 
+  const fetchCreditsHistory = async () => {
+    setSnackAlertData({
+      open: false,
+      message: "",
+      severity: "success",
+    });
+    try {
+      setLoading(true);
+      const response = await axiosInstance({
+        url: appUrl + "/getpurchasehistory",
+        method: "get",
+      });
+      setLoading(false);
+      if (response) {
+        setCreditsHistory(response?.data?.payments);
+        setSnackAlertData({
+          open: true,
+          message: response?.data?.message,
+          severity: "success",
+        });
+        if (response?.code > 200) {
+          setSnackAlertData({
+            open: true,
+            message: response?.message,
+            severity: "error",
+          });
+        }
+      }
+    } catch (error) {
+      setLoading(false);
+      setSnackAlertData({
+        open: true,
+        message: error.toString(),
+        severity: "error",
+      });
+    }
+  };
+  const fetchAnalyzeHistory = async () => {
+    setSnackAlertData({
+      open: false,
+      message: "",
+      severity: "success",
+    });
+    try {
+      const response = await axiosInstance({
+        url: appUrl + "/getuserhistory",
+        method: "get",
+      });
+      if (response) {
+        setAnalyzeHistory(response.data.Histories);
+        setSnackAlertData({
+          open: true,
+          message: response?.data?.message,
+          severity: "success",
+        });
+        if (response?.code > 200) {
+          setSnackAlertData({
+            open: true,
+            message: response?.message,
+            severity: "error",
+          });
+        }
+      }
+    } catch (error) {
+      setLoading(false);
+      setSnackAlertData({
+        open: true,
+        message: error.toString(),
+        severity: "error",
+      });
+    }
+  };
+  useEffect(() => {
+    fetchCreditsHistory();
+    fetchAnalyzeHistory()
+  }, []);
+
   return (
 
     <Box>
@@ -92,7 +178,7 @@ const Details = () => {
             gap: "2rem",
 
             marginTop: {
-              sm: "50px",
+              sm: "70px",
               xs: "65px",
             },
             flexDirection: {
@@ -132,7 +218,16 @@ const Details = () => {
       </Box>
 
       <Box
+        sx={{
+          display: "flex",
+          gap: "2rem",
+          marginTop: "10px",
+          flexDirection: {
+            xs: "column",
+            md: "row",
+          }
 
+        }}
       >
         <Box
           sx={{
@@ -159,7 +254,8 @@ const Details = () => {
             },
             "&::-webkit-scrollbar-thumb:hover": {
               background: "#b30000"
-            }
+            },
+            height: "500px"
           }}
         >
           <Typography
@@ -172,18 +268,62 @@ const Details = () => {
             Analyze History
           </Typography>
 
-         
+          {loading ? "loading...." : analyzeHistory.map((item, index) => (
             <GiftCard
-              // key={item._id}
-              // id={item._id}
-              title={"title"}
-              description={"description"}
-              // bullets={item.bullets}
-              // index={index}
-              // openModal={openModal}
+              key={item._id}
+              id={item._id}
+              title={item.title}
+              description={item.description}
+              bullets={item.bullets}
+              index={index}
+            // openModal={openModal}
             />
-         
+          ))}
         </Box>
+        <Box
+          sx={{
+            marginTop: "15px",
+            display: "flex",
+            gap: "1.8rem",
+            flexBasis: "50%",
+            padding: "24px 30px",
+            borderRadius: "10px",
+            flexDirection: "column",
+            maxHeight: "680px",
+            overflow: "auto",
+            boxShadow: "4px 5px 15px rgba(200, 200, 200, 0.61)",
+            "&::-webkit-scrollbar": {
+              width: "8px"
+            },
+            "&::-webkit-scrollbar-track": {
+              background: "#DFDFDF",
+              borderRadius: "10px"
+            },
+            "&::-webkit-scrollbar-thumb": {
+              background: "black",
+              borderRadius: "10px"
+            },
+            "&::-webkit-scrollbar-thumb:hover": {
+              background: "#b30000"
+            },
+            height: "500px"
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "27px",
+              fontWeight: "600",
+              color: "#333333",
+            }}
+          >
+            Credits History
+          </Typography>
+          {loading && creditsHistory.length < 1 ? "loading..." : creditsHistory.map((item, index) => (
+            <CreditsHistory item={item} key={index} index={index} />
+          ))}
+        </Box>
+
+
       </Box>
     </Box>
   );
