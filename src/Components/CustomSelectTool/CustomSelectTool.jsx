5730853ffde7c56b7c027759da27be6405feb381
Noor Amin <noor.amin@markcoders.com>
2024-08-07 02:10:13 +0500

    added into tool manaement thingsnee things

---
+++ src/Components/CustomSelectTool/CustomSelectTool.jsx
@@ -0,0 +1,81 @@
+import { Box, FormControl, InputLabel, MenuItem, Select, Typography } from '@mui/material';
+import React from 'react';
+
+const CustomSelectTool = ({ data = [], value, onChange, categoryError = "", boxShadow = "0px 8px 26px -4px rgba(0, 0, 0, 0.1)" }) => {
+  return (
+    <Box sx={{ mb: 2 }}>
+      <FormControl
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          padding: "0px 26px",
+          alignItems: "center",
+          justifyContent: "space-between",
+          borderRadius: "10px",
+          boxShadow: boxShadow,
+          width: '100%',
+          "& fieldset": { border: 'none' },
+          '& .MuiInputLabel-outlined.Mui-focused': {
+            fontSize: { lg: "20px" },
+            lineHeight: { lg: "30px" },
+            fontWeight: { lg: "500" },
+            color: "#A0A4A9",
+          },
+          position: "relative",
+        }}
+        variant="outlined"
+      >
+        <InputLabel
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: "26px",
+            transform: 'translateY(-50%)',
+            width: "100%",
+            fontSize: { lg: "20px" },
+            lineHeight: { lg: "30px" },
+            fontWeight: { lg: "500" },
+            color: "#A0A4A9",
+            display: value ? 'none' : 'flex',
+          }}
+          id="demo-simple-select-label"
+        >
+          Select Category
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          label="Select Category"
+          value={value}
+          onChange={onChange}
+          sx={{
+            width: "100%",
+            fontSize: { lg: "20px" },
+            lineHeight: { lg: "30px" },
+            fontWeight: { lg: "500" },
+            color: value ? "black" : "#A0A4A9",
+            "&::-webkit-scrollbar": { width: "8px" },
+            "&::-webkit-scrollbar-track": { background: "#DFDFDF", borderRadius: "10px" },
+            "&::-webkit-scrollbar-thumb": { background: "black", borderRadius: "10px" },
+            "&::-webkit-scrollbar-thumb:hover": { background: "#b30000" },
+          }}
+        >
+          {data.map((item, index) => (
+            <MenuItem key={index} value={item}>{item}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      {categoryError && (
+        <Typography sx={{
+          background: "whitesmoke",
+          p: "10px",
+          color: "red",
+          mt: "8px",
+          wordBreak: "break-word"
+        }}>{categoryError}</Typography>
+      )}
+    </Box>
+  );
+};
+
+export default CustomSelectTool;
--- src/Pages/ToolMangement/ToolManagement.jsx
+++ src/Pages/ToolMangement/ToolManagement.jsx
@@ -7,13 +7,13 @@ import CustomButton from "../../Components/CustomButton/CustomButton";
 import SearchIcon from "../../assets/images/searchIcon.png";
 import RestrictedKeyword from "../../Components/RestrictedKeyword/RestrictedKeyword";
 import axiosInstance from "../../Hooks/useQueryGallery/AuthHook/AuthHook";
-
+// import CustomSelect from "../../Components/CustomSelect/CustomSelect";
 import { handleSnackAlert } from "../../Redux/Slice/SnackAlertSlice/SnackAlertSlice";
 import { useSelector, useDispatch } from "react-redux";
 import LoaderMain from "../../Components/Loader/LoaderMain";
-import { get, isNumber } from "lodash";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import CustomSelectTool from "../../Components/CustomSelectTool/CustomSelectTool";
+CustomSelectTool
 
 const appUrl = import.meta.env.VITE_REACT_APP_API_URL;
 
@@ -22,12 +22,52 @@ const ToolManagement = () => {
   const [totalBullets, setTotalBullets] = useState(null);
   const [descriptionCharacters, setDescriptionCharacters] = useState(null);
   const [bulletcharacters, setBulletcharacters] = useState(null);
-  const [selectedFile, setSelectedFile] = useState(null);
   const navigate = useNavigate();
+  const [category, setCategory] = useState([
+    "Amazon Devices & Accessories",
+    "Amazon Renewed",
+    "Appliances",
+    "Apps & Games",
+    "Arts, Crafts & Sewing",
+    "Audible Books & Originals",
+    "Automotive",
+    "Baby",
+    "Beauty & Personal Care",
+    "Books",
+    "Camera & Photo Products",
+    "CDs & Vinyl",
+    "Cell Phones & Accessories",
+    "Clothing, Shoes & Jewelry",
+    "Collectible Coins",
+    "Computers & Accessories",
+    "Digital Educational Resources",
+    "Digital Music",
+    "Electronics",
+    "Entertainment Collectibles",
+    "Gift Cards",
+    "Grocery & Gourmet Food",
+    "Handmade Products",
+    "Health & Household",
+    "Home & Kitchen",
+    "Industrial & Scientific",
+    "Kindle Store",
+    "Kitchen & Dining",
+    "Movies & TV",
+    "Musical Instruments",
+    "Office Products",
+    "Patio, Lawn & Garden",
+    "Pet Supplies",
+    "Software",
+    "Sports & Outdoors",
+    "Sports Collectibles",
+    "Tools & Home Improvement",
+    "Toys & Games",
+    "Unique Finds",
+    "Video Games"
+  ]);
 
   const dispatch = useDispatch();
   const auth = useSelector((state) => state.auth);
-  // console.log(auth);
 
   const { control, handleSubmit, reset, getValues } = useForm({
     defaultValues: {
@@ -36,9 +76,12 @@ const ToolManagement = () => {
       descriptionCharacters: "",
       bulletcharacters: "",
       newKeyword: "",
-      instructions: ","
+      totalBulletsLength: "",
+      category: "",
+
     },
   });
+
   const [restrictedKeywords, setRestrictedKeywords] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredKeywords, setFilteredKeywords] = useState([]);
@@ -64,7 +107,7 @@ const ToolManagement = () => {
           method: "post",
           data: { word: data.newKeyword },
         });
-       
+
         console.log(response);
         if (response) {
           const updatedKeywords = [...filteredKeywords, data.newKeyword];
@@ -90,6 +133,7 @@ const ToolManagement = () => {
   };
 
   const onSubmit = async (data) => {
+    console.log(data);
     console.log("Input Data:", data);
 
     if (
@@ -109,6 +153,7 @@ const ToolManagement = () => {
             bulletNum: Number(data.totalBullets),
             bulletCharacters: Number(data.bulletcharacters),
             descriptionCharacters: Number(data.descriptionCharacters),
+            category: data.category
           },
         });
         dispatch(handleSnackAlert({ open: true, message: response.data.message, severity: "success" }));
@@ -118,6 +163,8 @@ const ToolManagement = () => {
           totalBullets: "",
           bulletcharacters: "",
           descriptionCharacters: "",
+          category: "",
+          totalBulletsLength:""
         });
         console.log(response);
       } catch (error) {
@@ -126,6 +173,7 @@ const ToolManagement = () => {
       }
     }
   };
+
   const fetchRules = async (data) => {
     try {
       const response = await axiosInstance({
@@ -150,13 +198,12 @@ const ToolManagement = () => {
         url: `${appUrl}/words`,
         method: "get",
       });
-      // console.log(response.data);
       setRestrictedKeywords(response.data);
       setFilteredKeywords(response.data);
       setLoading(false);
     } catch (error) {
       console.error("Error fetching restricted keywords:", error);
-      dispatch(handleSnackAlert({ open: true, message: error.response.data.message, severity: "error" }))
+      dispatch(handleSnackAlert({ open: true, message: error.response.data.message, severity: "error" }));
     }
   };
 
@@ -168,11 +215,9 @@ const ToolManagement = () => {
         params: { word: keyword },
       });
       console.log(response);
-      dispatch(handleSnackAlert({ open: true, message: response.data.message, severity: "success" }))
+      dispatch(handleSnackAlert({ open: true, message: response.data.message, severity: "success" }));
       if (response.status === 200) {
-        const updatedKeywords = restrictedKeywords.filter(
-          (kw) => kw !== keyword
-        );
+        const updatedKeywords = restrictedKeywords.filter((kw) => kw !== keyword);
         setRestrictedKeywords(updatedKeywords);
         setFilteredKeywords(
           updatedKeywords.filter((kw) =>
@@ -182,69 +227,55 @@ const ToolManagement = () => {
       }
     } catch (error) {
       console.error("Error removing keyword:", error);
-      dispatch(handleSnackAlert({ open: true, message: error.response.data.message, severity: "error" }))
+      dispatch(handleSnackAlert({ open: true, message: error.response.data.message, severity: "error" }));
     }
   };
-
-
 
   const downloadCsv = async () => {
     try {
       const response = await axiosInstance({
         url: `${appUrl}/csv`,
-        method: 'get',
-        responseType: 'blob', // Important to handle the response as a blob
+        method: "get",
+        responseType: "blob",
       });
 
-      // Create a URL for the file
-      const url = window.URL.createObjectURL(new Blob([response.data], { type: 'text/csv' }));
-
-      // Create a link element
-      const link = document.createElement('a');
+      const url = window.URL.createObjectURL(new Blob([response.data], { type: "text/csv" }));
+      const link = document.createElement("a");
       link.href = url;
-      link.setAttribute('download', 'data.csv'); // Set the download attribute with the filename
-
-      // Append the link to the body
+      link.setAttribute("download", "data.csv");
       document.body.appendChild(link);
-
-      // Programmatically click the link to trigger the download
       link.click();
-
-      // Clean up and remove the link
       link.parentNode.removeChild(link);
     } catch (error) {
-      console.error('Error downloading the CSV file:', error);
+      console.error("Error downloading the CSV file:", error);
     }
   };
+
   const handleFileChange = async (event) => {
     const file = event.target.files[0];
     setSelectedFile(file);
 
     if (file) {
       const formData = new FormData();
-      formData.append('file', file);
+      formData.append("file", file);
 
       try {
         const response = await axiosInstance({
           url: `${appUrl}/csv`,
-          method: 'post',
+          method: "post",
           data: formData,
           headers: {
-            'Content-Type': 'multipart/form-data',
+            "Content-Type": "multipart/form-data",
           },
         });
         console.log(response);
-        dispatch(handleSnackAlert({ open: true, message: response.data.message, severity: "success" }))
-
-        setFilteredKeywords(response.data.words)
+        dispatch(handleSnackAlert({ open: true, message: response.data.message, severity: "success" }));
+        setFilteredKeywords(response.data.words);
       } catch (error) {
-        console.error('Error uploading the file:', error);
+        console.error("Error uploading the file:", error);
       }
     }
   };
-
-
-
 
   useEffect(() => {
     fetchRestrictedKeywords();
@@ -274,7 +305,6 @@ const ToolManagement = () => {
               md: "row",
               xs: "column",
             },
-
           }}
         >
           <Box
@@ -283,12 +313,38 @@ const ToolManagement = () => {
               flexDirection: "column",
               gap: "0.5rem",
               flexBasis: "50%",
-              // position: "relative",
             }}
           >
-            <Box
-              sx={{ display: "flex", flexDirection: "column", gap: "0.4rem" }}
-            >
+              <Box sx={{ display: "flex", flexDirection: "column", gap: "0.4rem" }}>
+              <Heading Heading="Categories" />
+              <Controller
+                name="category"
+                control={control}
+                render={({ field }) => (
+                  <CustomSelectTool
+                    data={category}
+                    value={field.value}
+                    onChange={field.onChange}
+                  />
+                )}
+              />
+            </Box>
+            <Box sx={{ display: "flex", flexDirection: "column", gap: "0.4rem" }}>
+              <Heading Heading="Total Bullets Length" />
+              <Controller
+                name="totalBulletsLength"
+                control={control}
+                render={({ field }) => (
+                  <CustomInputShadow
+                    {...field}
+                    // placeholder={totalBulletsLength}
+                    onChange={(e) => field.onChange(e.target.value)}
+                    type={"number"}
+                  />
+                )}
+              />
+            </Box>
+            <Box sx={{ display: "flex", flexDirection: "column", gap: "0.4rem" }}>
               <Heading Heading="Title Characters" />
               <Controller
                 name="titleCharacters"
@@ -303,9 +359,7 @@ const ToolManagement = () => {
                 )}
               />
             </Box>
-            <Box
-              sx={{ display: "flex", flexDirection: "column", gap: "0.4rem" }}
-            >
+            <Box sx={{ display: "flex", flexDirection: "column", gap: "0.4rem" }}>
               <Heading Heading="Total Bullets" />
               <Controller
                 name="totalBullets"
@@ -320,9 +374,7 @@ const ToolManagement = () => {
                 )}
               />
             </Box>
-            <Box
-              sx={{ display: "flex", flexDirection: "column", gap: "0.4rem" }}
-            >
+            <Box sx={{ display: "flex", flexDirection: "column", gap: "0.4rem" }}>
               <Heading Heading="Bullet Characters" />
               <Controller
                 name="bulletcharacters"
@@ -337,9 +389,7 @@ const ToolManagement = () => {
                 )}
               />
             </Box>
-            <Box
-              sx={{ display: "flex", flexDirection: "column", gap: "0.4rem" }}
-            >
+            <Box sx={{ display: "flex", flexDirection: "column", gap: "0.4rem" }}>
               <Heading Heading="Description Characters" />
               <Controller
                 name="descriptionCharacters"
@@ -354,16 +404,15 @@ const ToolManagement = () => {
                 )}
               />
             </Box>
-
-
+          
             <Box
               sx={{
                 display: "flex",
                 mt: "15px",
                 justifyContent: {
                   md: "start",
-                  xs: "end"
-                }
+                  xs: "end",
+                },
               }}
             >
               <CustomButton
@@ -379,7 +428,6 @@ const ToolManagement = () => {
             </Box>
           </Box>
           <Box
-
             sx={{
               display: "flex",
               flexDirection: "column",
@@ -387,12 +435,10 @@ const ToolManagement = () => {
               flexBasis: "50%",
               mt: {
                 xs: "0px",
-                md: "0px"
-              }
+                md: "0px",
+              },
             }}
           >
-
-
             <Box
               sx={{
                 display: "flex",
@@ -401,23 +447,11 @@ const ToolManagement = () => {
                 padding: "24px 30px",
                 boxShadow: "4px 5px 15px 0px #C8C8C8 ",
                 borderRadius: "10px",
-
               }}
             >
               <Heading Heading="Restricted Keywords" />
-
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "1.3rem",
-                }}
-              >
-                <Box
-                  sx={{
-                    position: "relative",
-                  }}
-                >
+              <Box sx={{ display: "flex", flexDirection: "column", gap: "1.3rem" }}>
+                <Box sx={{ position: "relative" }}>
                   <input
                     type="search"
                     name="search"
@@ -447,7 +481,6 @@ const ToolManagement = () => {
                     }}
                   />
                 </Box>
-
                 <Box
                   sx={{
                     overflowY: "auto",
@@ -468,7 +501,6 @@ const ToolManagement = () => {
                     borderRadius: "10px",
                     marginTop: "10px",
                     height: "255px",
-                    // border:"2px solid red" 
                   }}
                 >
                   <Box
@@ -488,13 +520,7 @@ const ToolManagement = () => {
                     ))}
                   </Box>
                 </Box>
-
-                <Box
-                  sx={{
-                    position: "relative",
-                    marginTop: "30px",
-                  }}
-                >
+                <Box sx={{ position: "relative", marginTop: "30px" }}>
                   <Controller
                     name="newKeyword"
                     control={control}
@@ -524,13 +550,12 @@ const ToolManagement = () => {
                       position: "absolute",
                       top: {
                         sm: "7px",
-                        xs: "6px"
+                        xs: "6px",
                       },
                       right: {
                         xs: "05px",
-                        sm: "20px"
+                        sm: "20px",
                       },
-
                     }}
                   >
                     <CustomButton
@@ -546,7 +571,6 @@ const ToolManagement = () => {
                   </Typography>
                 </Box>
               </Box>
-
             </Box>
             <Box
               sx={{
@@ -555,8 +579,8 @@ const ToolManagement = () => {
                 gap: "1rem",
                 justifyContent: {
                   md: "start",
-                  xs: "end"
-                }
+                  xs: "end",
+                },
               }}
             >
               <CustomButton
@@ -569,36 +593,30 @@ const ToolManagement = () => {
                 background="linear-gradient(to right, #1A0049, #3F016A)"
                 onClick={downloadCsv}
               />
-
-
               <div>
                 <input
                   type="file"
                   id="fileInput"
                   onChange={handleFileChange}
                   style={{
-                    display: 'none',
+                    display: "none",
                   }}
                 />
                 <label
                   htmlFor="fileInput"
                   style={{
-                    display: 'inline-block',
-                    padding: '12px 20px',
-
-                    borderRadius: '12px',
+                    display: "inline-block",
+                    padding: "12px 20px",
+                    borderRadius: "12px",
                     background: "linear-gradient(to right, #1A0049, #3F016A)",
-                    color: '#333',
-                    cursor: 'pointer',
-                    color: "white"
-
-
+                    color: "#333",
+                    cursor: "pointer",
+                    color: "white",
                   }}
                 >
                   Select CSV File
                 </label>
               </div>
-
             </Box>
           </Box>
         </Box>
