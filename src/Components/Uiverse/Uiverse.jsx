d158dda5ad70a02964ea42e199e41d6503917791
Noor Amin <noor.amin@markcoders.com>
2024-09-09 20:46:42 +0500

    ...

--- src/Components/Uiverse/Uiverse.jsx
+++ src/Components/Uiverse/Uiverse.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Uiverse.css';
+import { Box } from '@mui/material';
 
 const Uiverse = ({ progress }) => {
   const percentage = typeof progress === 'string' ? parseFloat(progress) * 100 : progress;
   return (
+    <>
+    <Box sx={{mb:"3px", color:"#33333", fontSize:"0.8rem"}} >Analyzing</Box>
     <div className="loader1234" style={{ '--percentage': `${percentage}%` }} data-percentage={Math.round(percentage)}>
       {/* Removed inner div, as we use data-attribute for content now */}
     </div>
+    </>
   );
 };
 
